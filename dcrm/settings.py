fb6c3788fd79cc6b1e55c4e0709a6356e9d212e2
Adarsh Tripathi <adarsh29tripathi@gmail.com>
2024-04-22 11:05:14 +0530

    added few file in backend like serializers.py , urls.py

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'website',
 ]
 
@@ -51,6 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'dcrm.urls'
+
 
 TEMPLATES = [
     {
